ecc849fc62d197860a8e3e6a7ccb5645c55190f9
KamalBanjade <120898231+KamalBanjade@users.noreply.github.com>
2022-12-18 12:35:35 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+print("hey i am kamal banjade and i have started my python journey from today")
+#this is the comment line
+"""kamal
+banjade
+multiline 
+comment
+"""
+"""
+#this is print for two different print statements
+print("i am kamal" ,end=".")
+print("i am doing coding")
+#this is print for single line print statements here "," gives space
+print("i am kamal","i am doing coding",end="_")
+
+#\n is a line breaker and \t is a tab
+print("c:\n \"coding journey")#while giving location \ is must
+print("kamal is \n a good boy \t")
+#variable
+var1 ="kamal"
+print(type("var1"))
+
+var2="bimla"
+print(type(var2))
+print(var1+var2)
+
+var3="20"
+var4="30"
+#if we put"" insibe the print statement it will print exactly what is there and if we don't put it will print the conditions inside it
+#we can use int after print statement to calculate integer if it is string
+#similarly we have functions like str(), float()etc
+print(int(var3)+int(var4))
+#to print many times we have to do just multiply the numbwer we want suck like:
+print(10*"kamal banjade\n")
+#to multiply it must be in string
+var5="10"
+var6="30"
+print(10* str(int(var5+var6)))
+#intnum is variable here
+"""
+"""print("enter a number")
+intnum = input()
+print("you entered",int(intnum)+10)"""
+
+#sum of two numbers
+"""
+print("enter a first number")
+n1 = input()
+print("enter a second number")
+n2 = input()
+sum = int(n1)+int(n2)
+print("the sum is",sum)
+"""
+#difference of two number
+"""
+print("enter the first number")
+n3 = input()
+print("enter the second number")
+n4 = input()
+difference = int(n3)-int(n4)
+print("the difference is",difference)
+"""
+#product of two numbers
+"""
+print("enter the first number")
+n5 = input()
+print("enter the second number")
+n6 = input()
+product = int(n5)*int(n6)
+print("the product is",product)
+"""
+
+mystr = "kamal is a good person"
+print(mystr)
+
+
